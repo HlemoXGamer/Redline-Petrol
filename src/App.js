152455eb6718cd4a655ef75eff948bdb31bf0de6
HlemoXGamer <HlemoXGamer@Gmail.com>
2023-09-30 11:18:06 +0300

    hot fix on loading

--- src/App.js
+++ src/App.js
@@ -18,9 +18,9 @@ const darkTheme = createTheme({
 function App() {
     const [loading, setLoading] = useState(true);
     useEffect(() => {
-        setTimeout(() => {
+        window.onload = () => {
             setLoading(false);
-        }, 5000);
+        };
     }, []);
     return (
         <ThemeProvider theme={darkTheme}>
